81f544883849b3d04a336c629c74f2701278e918
vyskoutaris <vyskoutaris@hotmail.com>
2019-01-23 23:19:40 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
-function dwarfRollCall(dwarves) {
+var dwarves = ["Doc", "Dopey", "Bashful", "Grumpy"];
+
+function dwarfRollCall (dwarves){
+  var array = [];
+   for (var i = 0; i < dwarves.length; i++) {
+    array.push(`${i+1}. ${dwarves[i]} `)
+}
+   return array.join("")
 }
 
-function summonCaptainPlanet(planeteerCalls){
+var planeteerCalls = ["earth", "wind", "fire", "water", "heart"];
+
+function summonCaptainPlanet(planeteerCalls) {
+  return planeteerCalls.map(function(x) {
+  return x.toUpperCase() + "!";
+});
 }
 
 function longPlaneteerCalls(words) {
+ for (var i = 0; i < words.length; i++) {
+   if (words.length > 4)
+     return true;
+   }
+   return false;
 }
 
-function findTheCheese (foods) {
+
+
+function findTheCheese(foods) {
+  var indices = [];
+  var cheese = ["cheddar", "gouda", "camembert"];
+   	for(var i = 0; i < foods.length; i++) {
+   	var cheeseidx = cheese.indexOf(foods[i]);
+while (cheeseidx !== -1) {
+  indices.push(cheeseidx);
+   return foods[i];
+        }
+   	}
+    return 'no cheese!';
 }
