fa880f3fccc6978c9d5334cddc827f1052b275b6
vyskoutaris <vyskoutaris@hotmail.com>
2019-01-27 22:57:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 var dwarves = ["Doc", "Dopey", "Bashful", "Grumpy"];
 
 function dwarfRollCall (dwarves){
-  var array = [];
+  var arr = [];
    for (var i = 0; i < dwarves.length; i++) {
-    array.push(`${i+1}. ${dwarves[i]} `)
+    arr.push(`${i+1}. ${dwarves[i]} `)
 }
-   return array.join("")
+   return arr.join("")
 }
 
 var planeteerCalls = ["earth", "wind", "fire", "water", "heart"];
@@ -13,12 +13,13 @@ var planeteerCalls = ["earth", "wind", "fire", "water", "heart"];
 function summonCaptainPlanet(planeteerCalls) {
   return planeteerCalls.map(function(x) {
   return x.toUpperCase() + "!";
+  
 });
 }
 
 function longPlaneteerCalls(words) {
  for (var i = 0; i < words.length; i++) {
-   if (words.length > 4)
+   if (words[i].length > 4)
      return true;
    }
    return false;
@@ -27,14 +28,23 @@ function longPlaneteerCalls(words) {
 
 
 function findTheCheese(foods) {
-  var indices = [];
   var cheese = ["cheddar", "gouda", "camembert"];
    	for(var i = 0; i < foods.length; i++) {
    	var cheeseidx = cheese.indexOf(foods[i]);
-while (cheeseidx !== -1) {
-  indices.push(cheeseidx);
+if (cheeseidx != -1) {
    return foods[i];
         }
    	}
     return 'no cheese!';
+}
+
+// "bob".startsWith('b') => true, "jane".startsWith("b") => false
+function wordsWithB(words) {
+  var b = [];
+  for (var i = 0; i < words.length; i++) {
+    if (words[i].startsWith('b')){
+      b.push(words[i])
+    }
+  }
+  return b
 }
